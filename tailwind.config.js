ba2dbdd1a206742fc55ef0ffb9a5f86a7472b58c
levi04 <leviolevos@gmail.com>
2024-06-20 13:54:02 -0600

    Estilos

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./contenido/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#200616',
+        customcolor2: '#1B5299'
+      },
+    },
+  },
+  plugins: [],
+}
+
