38f45000c0e0333efa27504b750bb4ef3251a199
BootEmilio <emiorea8@gmail.com>
2024-06-24 12:11:08 -0600

    Mejoras visuales
    Mejoras visuales de las pantallas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         customColor: '#200616',
-        customcolor2: '#1B5299'
+        Demicolor2: '#373F51',
+        Demicolor: '#D7D9D7'
       },
     },
   },
-plugins: [],
+  plugins: [],
 }
 
