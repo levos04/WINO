2084ec260fda80c741813d5096731d1c73a50540
levi04 <leviolevos@gmail.com>
2024-06-26 01:17:56 -0600

    Entrenador

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.get('/InicioMiembro', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'InicioMiembro.html'));
 });
 
+app.get('/InicioEntrenador', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'InicioEntrenador.html'));
+});
+
 // Ruta para manejar el formulario
 app.post('/validarMiembro', (req, res) => {
   const datos = req.body;
@@ -74,6 +78,44 @@ app.post('/validarMiembro', (req, res) => {
       }
   });
 });
+
+app.post('/validarEntrenador', (req, res) => {
+    const datos = req.body;
+  
+    let nombre = datos.Nombre;
+    let apellido = datos.Apellido;
+    let fn = datos.FechaNacimiento;
+    let contacto = datos.Contacto;
+    let nu = datos.NombreUsuario;
+    let pass = datos.password;
+  
+    // Llamada al procedimiento almacenado
+    let registrar = "CALL agregar_entrenador(?, ?, ?, ?, ?, ?)";
+  
+    conexion.query(registrar, [nombre, apellido, fn, contacto, nu, pass], (error, results, fields) => {
+        if (error) {
+            console.error('Error ejecutando el procedimiento almacenado:', error);
+            if (error.code === 'ER_SIGNAL_EXCEPTION') {
+                return res.redirect('/registroFallido.html'); // Redirigir en caso de error específico
+            } else {
+                return res.redirect('/registroFallido.html'); // Redirigir en caso de otro error
+            }
+        }
+  
+        // Si la ejecución fue exitosa, verificar el mensaje devuelto por el procedimiento almacenado
+        if (results && results.length > 0 && results[0][0] && results[0][0].mensaje) {
+            let mensaje = results[0][0].mensaje;
+            if (mensaje.includes('Error')) {
+                return res.redirect('/registroFallido.html'); // Redirigir si hay un mensaje de error
+            } else {
+                return res.redirect('/registroExitoso.html'); // Redirigir si el cliente se agregó correctamente
+            }
+        } else {
+            console.error('Error: No se recibió un mensaje válido del procedimiento almacenado.');
+            return res.redirect('/registroFallido.html'); // Redirigir si no se recibió un mensaje válido
+        }
+    });
+  });
 
 app.listen(5500, () => {
     console.log('Servidor iniciado en http://localhost:5500');
