9ae2dba2176b72585a420fe3f8f55ee88e5321b5
levi04 <leviolevos@gmail.com>
2024-11-19 23:48:20 -0600

    Vuelven las páginas para las pruebas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +13,58 @@ app.use('/api/administradores', require('./WINO_Api/routes/administradores'));
 app.use('/api/clientes', require('./WINO_Api/routes/clientes'));
 app.use('/api/entrenadores', require('./WINO_Api/routes/entrenadores'));
 app.use('/api/gimnasios', require('./WINO_Api/routes/gimnasios'));
+
+// Servir archivos estáticos (CSS, JS, imágenes)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas para las vistas (HTML)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/gymCatalogo.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'gymCatalogo.html'));
+});
+
+app.get('/gymExito.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'gymExito.html'));
+});
+
+app.get('/gymFallo.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'gymFallo.html'));
+});
+
+app.get('/gymRegistro.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'gymRegistro.html'));
+});
+
+app.get('/miembroExito.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'miembroExito.html'));
+});
+
+app.get('/miembroFallo.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'miembroFallo.html'));
+});
+
+app.get('/miembroInicio.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'miembroInicio.html'));
+});
+
+app.get('/miembroRegistro.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'miembroRegistro.html'));
+});
+
+app.get('/Pregunta.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'Pregunta.html'));
+});
+
+app.get('/training.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'training.html'));
+});
+
+app.get('/welcome.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'welcome.html'));
+});
 
 // Manejo de errores
 app.use((req, res) => {
